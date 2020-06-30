6eab6504655c3a9c90ad92e0a9d2f2e97453068b
Matt Robertson <sile16@gmail.com>
2020-06-30 11:25:44 -0500

    adding fa 2.1, 2.2 & 1.19

--- rest_extract/purest_download.py
+++ rest_extract/purest_download.py
@@ -10,7 +10,7 @@ import sys
 
 fb_max_version = { 0:0, 1:9 }      #this is a list of major:minor_max version pairs
 pure1_max_version = { 0:-1, 1:0 }
-fa_2_max_version = { 2:0 }
+fa_2_max_version = { 2:2 }
 thread_count = 8
 baseURL = 'http://purest.dev.purestorage.com'
 spec_url = baseURL + '/pure-urls.js'
