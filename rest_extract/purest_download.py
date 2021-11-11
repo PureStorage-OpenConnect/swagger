fb0f918509cf2b01cf411c9e74bd454608d7bd52
Matt Robertson <sile16@gmail.com>
2021-11-11 16:33:55 -0600

    adding FB 2.2 spec

--- rest_extract/purest_download.py
+++ rest_extract/purest_download.py
@@ -7,7 +7,7 @@ import os
 import urllib
 import sys
 
-fb_max_version = { 0:0, 1:12, 2:1 }      #this is a list of major:minor_max version pairs
+fb_max_version = { 0:0, 1:12, 2:2 }      #this is a list of major:minor_max version pairs
 pure1_max_version = { 0:-1, 1:0 }
 fa_2_max_version = { 2:9 }
 thread_count = 8
