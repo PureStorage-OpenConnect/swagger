65332bad7311879e712bdcaccfd0ae862e487e95
Matt Robertson <sile16@gmail.com>
2022-08-31 19:34:19 -0500

    added comments to update all

--- rest_extract/update_all.sh
+++ rest_extract/update_all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-python3 convert_all_fa_pdf.py
+# there are no new pdf versions so don't update them.
+#python3 convert_all_fa_pdf.py
+
+#Pureset download update highest version mnumbers then run
 python3 purest_download.py
 python3 apply_templates.py
 python3 create_spec_index.py
