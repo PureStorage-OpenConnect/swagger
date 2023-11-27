d42f7ef6ae89bf8e470119b32ac95fe6dab28706
Matt Robertson <sile16@gmail.com>
2023-11-27 11:32:14 -0600

    adding FA 2.27 & 2.28

--- rest_extract/purest_download.py
+++ rest_extract/purest_download.py
@@ -10,7 +10,7 @@ import argparse
 
 fb_max_version = { 0:0, 1:12, 2:11 }      #this is a list of major:minor_max version pairs
 pure1_max_version = { 0:-1, 1:1 }
-fa_2_max_version = { 2:26 }
+fa_2_max_version = { 2:28 }
 thread_count = 8
 baseURL = 'http://purest.dev.purestorage.com'
 spec_url = baseURL + '/pure-urls.js'
