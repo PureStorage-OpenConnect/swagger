41405e85f7b28f27ac44e57f34e3707cd53b0d1a
Matt Robertson <sile16@gmail.com>
2025-02-10 15:15:09 -0600

    Update definitions directory Work with version 2.x

--- rest_extract/update_all.sh
+++ rest_extract/update_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Get the latest files from artfactory: https://artifactory.pstg-inf.net/ui/repos/tree/General/virtual-pure-shared-purest-maven/release/com/purestorage/purest"
 
-echo "And extract to definitions/oas2 & definitions/oas3"
+echo "And extract to definitions/oas2.0 & definitions/oas3.0 (not used yet)"
 read -p "Press enter to continue"
 
 python3 apply_templates.py
